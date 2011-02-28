27e39add018e29f2eab91fa33fa6aceace7fc1fc
Joe Thornber <joe.thornber@gmail.com>
2011-02-28 15:41:17 +0000

    mmd open/close tests

--- multisnap-metadata-test.c
+++ multisnap-metadata-test.c
@@ -4,6 +4,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+#include "md/persistent-data/block-manager.h"
 #include "md/multisnap-metadata.h"
 
 /*----------------------------------------------------------------*/
@@ -11,211 +12,231 @@
 #define NR_BLOCKS 1024
 #define BM_BLOCK_SIZE 4096
 #define CACHE_SIZE 16
+#define METADATA_BLOCK_SIZE 4096
 #define DATA_BLOCK_SIZE ((1024 * 1024 * 128) >> SECTOR_SHIFT)
 
 #define DATA_DEV_SIZE 10000
-
-typedef int (*test_fn)(struct multisnap_metadata *tpm);
+#define TEST_DEVICE "/dev/sdc"	/* FIXME: get this from module parameters */
 
 /*----------------------------------------------------------------*/
 
-static int check_empty_transaction(struct multisnap_metadata *tpm)
-{
-	return multisnap_metadata_commit(tpm);
-}
-
-static int check_insert_1_transaction(struct multisnap_metadata *tpm)
+static int with_block(const char *path, block_t blk,
+		      void (*fn)(void *, void *),
+		      void *context)
 {
 	int r;
-	static block_t map[DATA_DEV_SIZE];
-	block_t tb;
-
-	for (tb = 0; tb < DATA_DEV_SIZE; tb++) {
-		block_t pb;
-
-		r = multisnap_metadata_insert(tpm, 0, tb, map + tb);
-		if (r < 0) {
-			printk(KERN_ALERT "insert failed");
-			return r;
-		}
-
-		/* check we can lookup straight away */
-		r = multisnap_metadata_lookup(tpm, 0, tb, 1, &pb);
-		if (r < 0) {
-			printk(KERN_ALERT "first lookup failed (%u)", (unsigned) tb);
-			return r;
-		}
-
-		if (pb != map[tb]) {
-			printk(KERN_ALERT "first comparison failed");
-			return -1;
-		}
+	struct block_manager *bm;
+	struct block_device *bdev;
+	struct block *b;
+	int mode = FMODE_READ | FMODE_WRITE | FMODE_EXCL;
+	bdev = blkdev_get_by_path(path, mode, &with_block);
+	if (IS_ERR(bdev)) {
+		printk(KERN_ALERT "blkdev_get_by_path failed");
+		return -1;
 	}
 
-	r = multisnap_metadata_commit(tpm);
-	if (r < 0) {
-		printk(KERN_ALERT "commit failed");
-		return r;
-	}
-	printk(KERN_ALERT "%u inserts in a single commit",
-	       DATA_DEV_SIZE);
-
-	/* check we can lookup after a commit */
-	for (tb = 0; tb < DATA_DEV_SIZE; tb++) {
-		block_t pb;
-
-		r = multisnap_metadata_lookup(tpm, 0, tb, 1, &pb);
-		if (r < 0) {
-			printk(KERN_ALERT "thinp-metadata_lookup failed for block %u",
-			       (unsigned) tb);
-			return r;
-		}
-
-		if (pb != map[tb]) {
-			printk(KERN_ALERT "unexpected mapping");
-			return -1;
-		}
+	bm = block_manager_create(bdev, METADATA_BLOCK_SIZE, 1);
+	if (!bm) {
+		printk(KERN_ALERT "%s: couldn't create bm", __func__);
+		return -1;
 	}
 
+	r = bm_write_lock(bm, blk, &b);
+	if (r)
+		printk(KERN_ALERT "%s: couldn't lock block",
+		       __func__);
+	else {
+		fn(context, block_data(b));
+		bm_unlock(b);
+		r = bm_flush(bm, 1);
+	}
+
+	block_manager_destroy(bm);
+	blkdev_put(bdev, mode);
+	return r;
+}
+
+/*--------------------------------*/
+
+static void memset_(void *context, void *data)
+{
+	unsigned char *v = (unsigned char *) context;
+	memset(data, *v, METADATA_BLOCK_SIZE);
+}
+
+static int memset_block(const char *path, block_t blk, unsigned char v)
+{
+	return with_block(path, blk, memset_, &v);
+}
+
+/*--------------------------------*/
+
+struct sb_context {
+	size_t offset;
+	unsigned char v;
+};
+
+static void set_byte_(void *context, void *data)
+{
+	unsigned char *as_chars = data;
+	struct sb_context *sbc = (struct sb_context *) context;
+	as_chars[sbc->offset] = sbc->v;
+}
+
+static int set_block_byte(const char *path, block_t blk, size_t offset, unsigned char v)
+{
+	struct sb_context sbc;
+	sbc.offset = offset;
+	sbc.v = v;
+	return with_block(path, blk, set_byte_, &sbc);
+}
+
+/*--------------------------------*/
+
+struct test_context {
+	struct block_device *bdev;
+	struct multisnap_metadata *mmd;
+};
+
+static int create_mmd(struct test_context *tc)
+{
+	int mode = FMODE_READ | FMODE_WRITE | FMODE_EXCL;
+
+	tc->bdev = blkdev_get_by_path(TEST_DEVICE, mode, &create_mmd);
+	if (IS_ERR(tc->bdev))
+		return -1;
+
+	tc->mmd = multisnap_metadata_open(tc->bdev,
+					  DATA_BLOCK_SIZE,
+					  DATA_DEV_SIZE);
+	if (!tc->mmd) {
+		blkdev_put(tc->bdev, mode);
+		printk(KERN_ALERT "couldn't create mmd");
+		return -1;
+	}
 
 	return 0;
 }
 
-static int check_insert_multi_transaction(struct multisnap_metadata *tpm)
+static int destroy_mmd(struct test_context *tc)
 {
-	int r;
-	static block_t map[DATA_DEV_SIZE];
-	block_t tb;
+	int mode = FMODE_READ | FMODE_WRITE | FMODE_EXCL;
 
-	for (tb = 0; tb < DATA_DEV_SIZE; tb++) {
-		block_t pb;
-
-		r = multisnap_metadata_insert(tpm, 0, tb, map + tb);
-		if (r < 0)
-			return r;
-
-		/* check we can lookup straight away */
-		r = multisnap_metadata_lookup(tpm, 0, tb, 1, &pb);
-		if (r < 0)
-			return r;
-
-		if (pb != map[tb])
-			return -1;
-
-		r = multisnap_metadata_commit(tpm);
-		if (r < 0)
-			return r;
-	}
-
-	/* check we can lookup after a commit */
-	for (tb = 0; tb < DATA_DEV_SIZE; tb++) {
-		block_t pb;
-
-		r = multisnap_metadata_lookup(tpm, 0, tb, 1, &pb);
-		if (r < 0) {
-			printk(KERN_ALERT "thinp-metadata_lookup failed for block %u",
-			       (unsigned) tb);
-			return r;
-		}
-
-		if (pb != map[tb]) {
-			printk(KERN_ALERT "unexpected mapping");
-			return -1;
-		}
-	}
-
-	printk(KERN_ALERT "%u insert/commit cycles",
-	       DATA_DEV_SIZE);
-
-	return 0;
-}
-
-static int check_accessors(struct multisnap_metadata *tpm)
-{
-	int r;
-	sector_t s;
-	block_t b;
-
-	r = multisnap_metadata_get_data_block_size(tpm, 0, &s);
-	if (r < 0) {
-		printk(KERN_ALERT "get_data_block_size() failed");
-		return r;
-	}
-
-	if (s != DATA_BLOCK_SIZE) {
-		printk(KERN_ALERT "data block size incorrect");
-		return -1;
-	}
-
-	r = multisnap_metadata_get_data_dev_size(tpm, 0, &b);
-	if (r < 0) {
-		printk(KERN_ALERT "get_data_dev_size failed");
-		return -1;
-	}
-
-	if (b != DATA_DEV_SIZE) {
-		printk(KERN_ALERT "data dev size incorrect");
-		return -1;
-	}
-
-	r = multisnap_metadata_get_provisioned_blocks(tpm, 0, &b);
-	if (r < 0) {
-		printk(KERN_ALERT "data_first_free() failed");
-		return -1;
-	}
-
-	if (b != 0) {
-		printk(KERN_ALERT "data first free incorrect");
-		return -1;
-	}
-
-	r = multisnap_metadata_resize_data_dev(tpm, 0, 101);
-	if (r < 0) {
-		printk(KERN_ALERT "resize_data_dev() failed");
-		return -1;
-	}
-
-	r = multisnap_metadata_get_data_dev_size(tpm, 0, &b);
-	if (r < 0) {
-		printk(KERN_ALERT "get_data_dev_size failed(2)");
-		return -1;
-	}
-
-	if (b != 101) {
-		printk(KERN_ALERT "data dev size incorrect(2)");
-		return -1;
-	}
-
+	multisnap_metadata_close(tc->mmd);
+	blkdev_put(tc->bdev, mode);
 	return 0;
 }
 
 /*----------------------------------------------------------------*/
+
+static int check_create_mmd(void)
+{
+	int r;
+	struct test_context tc;
+
+	r = memset_block(TEST_DEVICE, 0, 0);
+	if (r)
+		return r;
+
+	r = create_mmd(&tc);
+	if (r)
+		return r;
+
+	return destroy_mmd(&tc);
+}
+
+static int check_reopen_mmd(void)
+{
+	int r;
+	struct test_context tc;
+
+	r = memset_block(TEST_DEVICE, 0, 0);
+	if (r)
+		return r;
+
+	r = create_mmd(&tc);
+	if (r)
+		return r;
+
+	r = destroy_mmd(&tc);
+	if (r)
+		return r;
+
+	r = create_mmd(&tc);
+	if (r)
+		return r;
+
+	return destroy_mmd(&tc);
+}
+
+static int check_reopen_bad_fails(void)
+{
+	int r;
+	struct test_context tc;
+
+	r = memset_block(TEST_DEVICE, 0, 63);
+	if (r)
+		return r;
+
+	r = create_mmd(&tc);
+	if (!r) {
+		printk(KERN_ALERT "create_mmd unexpectedly succeeded");
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int check_reopen_slightly_bad_fails(void)
+{
+	int r;
+	struct test_context tc;
+
+	/* setup a valid mmd */
+	r = memset_block(TEST_DEVICE, 0, 0);
+	if (r) {
+		printk(KERN_ALERT "memset failed");
+		return r;
+	}
+
+	r = create_mmd(&tc);
+	if (r)
+		return r;
+
+	r = destroy_mmd(&tc);
+	if (r)
+		return r;
+
+	/*
+	 * Touch just one byte, quite far into the block, so it's probably
+	 * not used.
+	 */
+	set_block_byte(TEST_DEVICE, 0, 1024, 63);
+
+	r = create_mmd(&tc);
+	if (!r) {
+		printk(KERN_ALERT "create_mmd unexpectedly succeeded");
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return 0;
+}
+
+/*----------------------------------------------------------------*/
+
+typedef int (*test_fn)(void);
 
 static int run_test(const char *name, test_fn fn)
 {
 	int r;
-	struct multisnap_metadata *tpm;
-	struct block_device *bdev;
-	int mode = FMODE_READ | FMODE_WRITE | FMODE_EXCL;
-	bdev = blkdev_get_by_path("/dev/sdb", mode, &run_test);
-	if (IS_ERR(bdev))
-		return -1;
-
-	tpm = multisnap_metadata_open(bdev,
-				      DATA_BLOCK_SIZE,
-				      DATA_DEV_SIZE);
-	if (!tpm) {
-		printk(KERN_ALERT "couldn't create tpm");
-		return -1;
-	}
 
 	printk(KERN_ALERT "running %s ... ", name);
-	r = fn(tpm);
+	r = fn();
 	printk(r == 0 ? KERN_ALERT "pass\n" : KERN_ALERT "fail\n");
 
-	multisnap_metadata_close(tpm);
-	blkdev_put(bdev, mode);
-	return 0;
+	return r;
 }
 
 static int multisnap_metadata_test_init(void)
@@ -224,18 +245,18 @@ static int multisnap_metadata_test_init(void)
 		const char *name;
 		test_fn fn;
 	} table_[] = {
-		{"empty transaction", check_empty_transaction},
-		{"check multiple inserts within one transaction", check_insert_1_transaction},
-		{"check 1 insert per transaction", check_insert_multi_transaction},
-		{"checking accessor functions", check_accessors}
+		{"create new metadata device",		check_create_mmd},
+		{"reopen metadata device",		check_reopen_mmd},
+		{"reopen a bad superblock",		check_reopen_bad_fails},
+		{"reopen a slightly bad superblock",	check_reopen_slightly_bad_fails}
 	};
 
-	int i;
+	int i, r = 0;
 
 	for (i = 0; i < sizeof(table_) / sizeof(*table_); i++)
-		run_test(table_[i].name, table_[i].fn);
+		r |= run_test(table_[i].name, table_[i].fn);
 
-	return 0;
+	return r;
 }
 
 static void multisnap_metadata_test_exit(void)
