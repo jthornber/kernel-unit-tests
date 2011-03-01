0ec0f519a30b15f35bbea1b3a7529d153af19a97
Joe Thornber <joe.thornber@gmail.com>
2011-03-01 14:11:08 +0000

    a bunch more mmd tests

--- multisnap-metadata-test.c
+++ multisnap-metadata-test.c
@@ -15,7 +15,7 @@
 #define METADATA_BLOCK_SIZE 4096
 #define DATA_BLOCK_SIZE ((1024 * 1024 * 128) >> SECTOR_SHIFT)
 
-#define DATA_DEV_SIZE 10000
+#define DATA_DEV_SIZE 512
 #define TEST_DEVICE "/dev/sdc"	/* FIXME: get this from module parameters */
 
 /*----------------------------------------------------------------*/
@@ -96,9 +96,11 @@ static int set_block_byte(const char *path, block_t blk, size_t offset, unsigned
 
 /*--------------------------------*/
 
+#define MAX_MSD 32
 struct test_context {
 	struct block_device *bdev;
 	struct multisnap_metadata *mmd;
+	struct ms_device *msd[MAX_MSD];
 };
 
 static int create_mmd(struct test_context *tc)
@@ -123,10 +125,84 @@ static int create_mmd(struct test_context *tc)
 
 static int destroy_mmd(struct test_context *tc)
 {
+	int r;
 	int mode = FMODE_READ | FMODE_WRITE | FMODE_EXCL;
+	unsigned i;
 
-	multisnap_metadata_close(tc->mmd);
+	for (i = 0; i < MAX_MSD; i++) {
+		if (tc->msd[i]) {
+			r = multisnap_metadata_close_device(tc->msd[i]);
+			if (r) {
+				printk(KERN_ALERT "mmd_close_device failed");
+				return r;
+			}
+		}
+	}
+
+	r = multisnap_metadata_close(tc->mmd);
+	if (r)
+		return r;
+
 	blkdev_put(tc->bdev, mode);
+	return 0;
+}
+
+/*--------------------------------*/
+
+static int setup_fresh_mmd(struct test_context *tc)
+{
+	int r = memset_block(TEST_DEVICE, 0, 0);
+	if (r) {
+		printk(KERN_ALERT "memset failed");
+		return r;
+	}
+
+	memset(tc, 0, sizeof(*tc));
+	return create_mmd(tc);
+}
+
+static int setup_fresh_mmd_and_thin(struct test_context *tc,
+				    multisnap_dev_t id)
+{
+	int r;
+
+	r = setup_fresh_mmd(tc);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_create_thin(tc->mmd, id);
+	if (r) {
+		printk(KERN_ALERT "mmd_create_thin failed");
+		destroy_mmd(tc);
+	}
+
+	return r;
+}
+
+static int setup_fresh_and_open_thins(struct test_context *tc, unsigned count)
+{
+	int r;
+	unsigned i;
+
+	r = setup_fresh_mmd(tc);
+	if (r)
+		return r;
+
+	for (i = 0; i < count; i++) {
+		r = multisnap_metadata_create_thin(tc->mmd, i);
+		if (r) {
+			printk(KERN_ALERT "mmd_create_thin failed");
+			destroy_mmd(tc);
+		}
+
+		r = multisnap_metadata_open_device(tc->mmd, i, tc->msd + i);
+		if (r) {
+			printk(KERN_ALERT "mmd open_device failed");
+			destroy_mmd(tc);
+			return r;
+		}
+	}
+
 	return 0;
 }
 
@@ -137,11 +213,7 @@ static int check_create_mmd(void)
 	int r;
 	struct test_context tc;
 
-	r = memset_block(TEST_DEVICE, 0, 0);
-	if (r)
-		return r;
-
-	r = create_mmd(&tc);
+	r = setup_fresh_mmd(&tc);
 	if (r)
 		return r;
 
@@ -153,11 +225,7 @@ static int check_reopen_mmd(void)
 	int r;
 	struct test_context tc;
 
-	r = memset_block(TEST_DEVICE, 0, 0);
-	if (r)
-		return r;
-
-	r = create_mmd(&tc);
+	r = setup_fresh_mmd(&tc);
 	if (r)
 		return r;
 
@@ -196,14 +264,7 @@ static int check_reopen_slightly_bad_fails(void)
 	int r;
 	struct test_context tc;
 
-	/* setup a valid mmd */
-	r = memset_block(TEST_DEVICE, 0, 0);
-	if (r) {
-		printk(KERN_ALERT "memset failed");
-		return r;
-	}
-
-	r = create_mmd(&tc);
+	r = setup_fresh_mmd(&tc);
 	if (r)
 		return r;
 
@@ -227,6 +288,367 @@ static int check_reopen_slightly_bad_fails(void)
 	return 0;
 }
 
+static int check_open_bad_msd(void)
+{
+	int r;
+	struct test_context tc;
+	struct ms_device *msd;
+
+	r = setup_fresh_mmd(&tc);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_open_device(tc.mmd, 0, &msd);
+	if (!r) {
+		printk(KERN_ALERT "open msd unexpectedly succeeded");
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_create_thin_msd(void)
+{
+	int r;
+	struct test_context tc;
+	r = setup_fresh_mmd_and_thin(&tc, 0);
+	if (r)
+		return r;
+
+	return destroy_mmd(&tc);
+}
+
+static int check_open_thin_msd(void)
+{
+	int r;
+	struct ms_device *msd;
+	struct test_context tc;
+
+	r = setup_fresh_mmd_and_thin(&tc, 0);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_open_device(tc.mmd, 0, &msd);
+	if (r) {
+		printk(KERN_ALERT "mmd_open_device failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = multisnap_metadata_close_device(msd);
+	if (r) {
+		printk(KERN_ALERT "mmd_close_device failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_open_msd_twice_fails(void)
+{
+	int r;
+	struct ms_device *msd, *msd2;
+	struct test_context tc;
+
+	r = setup_fresh_mmd_and_thin(&tc, 0);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_open_device(tc.mmd, 0, &msd);
+	if (r) {
+		printk(KERN_ALERT "mmd_open_device failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = multisnap_metadata_open_device(tc.mmd, 0, &msd2);
+	if (!r) {
+		printk(KERN_ALERT "mmd_open_device (for the second time) unexpected succeeded");
+		multisnap_metadata_close_device(msd);
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = multisnap_metadata_close_device(msd);
+	if (r) {
+		printk(KERN_ALERT "mmd_close_device failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_mmd_close_with_open_msd_fails(void)
+{
+	int r;
+	struct ms_device *msd;
+	struct test_context tc;
+
+	r = setup_fresh_mmd_and_thin(&tc, 0);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_open_device(tc.mmd, 0, &msd);
+	if (r) {
+		printk(KERN_ALERT "mmd_open_device failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = destroy_mmd(&tc);
+	if (!r) {
+		printk(KERN_ALERT "destroy_mmd() unexpectedly succeeded");
+		return -1;
+	}
+
+	/* tidy */
+	r = multisnap_metadata_close_device(msd);
+	if (r) {
+		printk(KERN_ALERT "mmd_close_device failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_delete_msd(void)
+{
+	int r;
+	struct test_context tc;
+
+	r = setup_fresh_mmd_and_thin(&tc, 0);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_delete(tc.mmd, 0);
+	if (r) {
+		printk(KERN_ALERT "mmd_delete failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_open_of_deleted_msd_fails(void)
+{
+	int r;
+	struct ms_device *msd;
+	struct test_context tc;
+
+	r = setup_fresh_mmd_and_thin(&tc, 0);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_delete(tc.mmd, 0);
+	if (r) {
+		printk(KERN_ALERT "mmd_delete failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = multisnap_metadata_open_device(tc.mmd, 0, &msd);
+	if (!r) {
+		printk(KERN_ALERT "mmd open_device unexpectedly succeeded");
+		multisnap_metadata_close_device(msd);
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_empty_msd_lookup_fails(void)
+{
+	int r;
+	struct test_context tc;
+	uint64_t value;
+
+	r = setup_fresh_and_open_thins(&tc, 1);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_lookup(tc.msd[0], 0, 1, &value);
+	if (!r) {
+		printk(KERN_ALERT "mmd_lookup unexpectedly succeeded");
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_insert_succeeds(void)
+{
+	int r;
+	struct test_context tc;
+	uint64_t value;
+
+	r = setup_fresh_and_open_thins(&tc, 1);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_insert(tc.msd[0], 0, &value);
+	if (r) {
+		printk(KERN_ALERT "mmd_insert failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_two_inserts_in_same_device_differ(void)
+{
+	int r;
+	struct test_context tc;
+	uint64_t value1, value2;
+
+	r = setup_fresh_and_open_thins(&tc, 1);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_insert(tc.msd[0], 0, &value1);
+	if (r) {
+		printk(KERN_ALERT "mmd_insert failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = multisnap_metadata_insert(tc.msd[0], 1, &value2);
+	if (r) {
+		printk(KERN_ALERT "mmd_insert failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	if (value1 == value2) {
+		printk(KERN_ALERT "mmd_inserts mapped to same destination");
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_two_inserts_in_different_devices_differ(void)
+{
+	int r;
+	struct test_context tc;
+	uint64_t value1, value2;
+
+	r = setup_fresh_and_open_thins(&tc, 2);
+	if (r)
+		return r;
+
+	r = multisnap_metadata_insert(tc.msd[0], 0, &value1);
+	if (r) {
+		printk(KERN_ALERT "mmd_insert failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	r = multisnap_metadata_insert(tc.msd[1], 0, &value2);
+	if (r) {
+		printk(KERN_ALERT "mmd_insert failed");
+		destroy_mmd(&tc);
+		return r;
+	}
+
+	if (value1 == value2) {
+		printk(KERN_ALERT "mmd_inserts mapped to same destination");
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_data_space_can_be_exhausted(void)
+{
+	int r;
+	struct test_context tc;
+	uint64_t value;
+	unsigned i;
+
+	r = setup_fresh_and_open_thins(&tc, 1);
+	if (r)
+		return r;
+
+	/* use up all the available data space */
+	for (i = 0; i < DATA_DEV_SIZE; i++) {
+		r = multisnap_metadata_insert(tc.msd[0], i, &value);
+		if (r) {
+			printk(KERN_ALERT "mmd_insert failed");
+			destroy_mmd(&tc);
+			return r;
+		}
+	}
+
+	/* next insert should fail */
+	r = multisnap_metadata_insert(tc.msd[0], i, &value);
+	if (!r) {
+		printk(KERN_ALERT "insert unexpectedly succeeded");
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_data_space_can_be_exhausted_two_devs(void)
+{
+	int r;
+	struct test_context tc;
+	uint64_t value;
+	unsigned i;
+
+	r = setup_fresh_and_open_thins(&tc, 2);
+	if (r)
+		return r;
+
+	/* use up all the available data space */
+	for (i = 0; i < DATA_DEV_SIZE; i++) {
+		r = multisnap_metadata_insert(tc.msd[i % 2], i, &value);
+		if (r) {
+			printk(KERN_ALERT "mmd_insert failed");
+			destroy_mmd(&tc);
+			return r;
+		}
+	}
+
+	/* next insert should fail */
+	r = multisnap_metadata_insert(tc.msd[i % 2], i, &value);
+	if (!r) {
+		printk(KERN_ALERT "insert unexpectedly succeeded");
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
+static int check_get_workqueue(void)
+{
+	int r;
+	struct test_context tc;
+	struct workqueue_struct *wq;
+
+	r = setup_fresh_and_open_thins(&tc, 1);
+	if (r)
+		return r;
+
+	wq = multisnap_metadata_get_workqueue(tc.msd[0]);
+	if (!wq) {
+		printk(KERN_ALERT "get workqueue failed");
+		destroy_mmd(&tc);
+		return -1;
+	}
+
+	return destroy_mmd(&tc);
+}
+
 /*----------------------------------------------------------------*/
 
 typedef int (*test_fn)(void);
@@ -248,16 +670,41 @@ static int multisnap_metadata_test_init(void)
 		const char *name;
 		test_fn fn;
 	} table_[] = {
-		{"create new metadata device",		check_create_mmd},
-		{"reopen metadata device",		check_reopen_mmd},
-		{"reopen a bad superblock",		check_reopen_bad_fails},
-		{"reopen a slightly bad superblock",	check_reopen_slightly_bad_fails}
+		/* creation of the metadata device */
+		{"create new metadata device",	     check_create_mmd},
+		{"reopen metadata device",	     check_reopen_mmd},
+		{"reopen a bad superblock",	     check_reopen_bad_fails},
+		{"reopen a slightly bad superblock", check_reopen_slightly_bad_fails},
+
+		/* creation of virtual devices within the mmd */
+		{"open non existent virtual device fails",	check_open_bad_msd},
+		{"create a thin virtual device succeeds",	check_create_thin_msd},
+		{"open existing virtual device succeeds",	check_open_thin_msd},
+		{"open existing virtual device twice fails",	check_open_msd_twice_fails},
+		{"mmd close with open devices fails",		check_mmd_close_with_open_msd_fails},
+		{"delete a thin virtual device succeeds",	check_delete_msd},
+
+		// waiting for btree_remove()
+		// {"opening a deleted virtual device fails",   check_open_of_deleted_msd_fails},
+
+		{"lookup of empty virtual device fails",	check_empty_msd_lookup_fails},
+		{"insert of a new mapping succeeds",		check_insert_succeeds},
+		{"two inserted mappings differ (same dev)",	check_two_inserts_in_same_device_differ},
+		{"two inserted mappings differ (diff devs)",	check_two_inserts_in_different_devices_differ},
+
+		{"data space may be exhausted",			check_data_space_can_be_exhausted},
+		{"data space may be exhausted (2 devs)",	check_data_space_can_be_exhausted_two_devs},
+
+		{"get workqueue", check_get_workqueue},
 	};
 
-	int i, r = 0;
+	int i, r;
 
-	for (i = 0; i < sizeof(table_) / sizeof(*table_); i++)
-		r |= run_test(table_[i].name, table_[i].fn);
+	for (i = 0; i < sizeof(table_) / sizeof(*table_); i++) {
+		r = run_test(table_[i].name, table_[i].fn);
+		if (r)
+			break;
+	}
 
 	return r;
 }
